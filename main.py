71848b58018fc6a25a87eac0073701038614bfe0
Abhishek Kashyap <abhishek.kashyap@sambanovasystems.com>
2023-09-19 00:48:25 +0530

    Initial commit for OAuth Callback Service
    This commit adds all the required files to create a Docker image and
    deploy the callback service to kubernetes. This service is supposed to
    be helpful in observing the different steps of OAuth2 flow.
    
    Signed-off-by: Abhishek Kashyap <abhishek.kashyap@sambanovasystems.com>

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+
+@app.route('/callback')
+def callback():
+    # Capture request details
+    log_data = {
+        "method": request.method,
+        "path": request.full_path,
+        "headers": dict(request.headers),
+        "arguments": request.args,
+        "body": request.data.decode('utf-8')  # assuming the body data is utf-8 encoded
+    }
+
+    app.logger.info(f"Received request: {log_data}")
+
+    code = request.args.get('code')
+    # Here, you'd typically exchange the 'code' for an access token.
+    return f"Received code: {code}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
